a9844c3d4838b6bcd07488843ec1cdf892359337
João Rafael Broz dos Santos <joao.rafael@al.infnet.edu.br>
2023-07-25 10:28:17 -0300

    comit

---
+++ script.js
@@ -0,0 +1,16 @@
+const objs = [
+    {
+
+        "nome": "Pedro",
+        "idade": 20,
+        "esta_trabalhando": true,
+        "detalhes_profissao": {
+    
+            "profissao": "programador",
+            "empresa": "x"
+    
+        },
+    
+       "hobbies": [1,"a", "verdade"]
+    }
+]
